16137444a2f113e8b1d4ad42ab7eb3ade8eae503
David <dheis1337@gmail.com>
2018-04-28 11:21:00 -0600

    make Cabin a factor

--- Titanic/Cleaning.R
+++ Titanic/Cleaning.R
@@ -8,110 +8,114 @@ library(data.table)
 setwd("C:/MyStuff/DataScience/Projects/Kaggle/Titanic/")
 
 # Read in data
-train <- fread("train.csv", stringsAsFactors = FALSE)
-test <- fread("test.csv", stringsAsFactors = FALSE)
+dat <- fread("train.csv", stringsAsFactors = FALSE)
+sub <- fread("test.csv", stringsAsFactors = FALSE)
 
 
 # I want to change the Survived, and Pclass variables to factor now
-train[, Survived := factor(Survived)]
-train[, Pclass := factor(Pclass)]
-train[, Sex := factor(Sex)]
+dat[, Survived := factor(Survived)]
+dat[, Pclass := factor(Pclass)]
+dat[, Sex := factor(Sex)]
 
 # The Embarked variable can be dropped, as I found it unimportant in the EDA.R
 # script
-train[, Embarked := NULL]
+dat[, Embarked := NULL]
 
 # Now I will impute the missing Age values and create two new columns for imputations. 
 # One will be a mean imputation and the other a median. 
-ages <- train[, Age]
+ages <- dat[, Age]
 mean.ages <- mean(ages, na.rm = TRUE)
 median.ages <- median(ages, na.rm = TRUE)
 
 
 # Make age.median and age.mean column
-train[, age.median := Age]
-train[, age.mean := Age]
+dat[, age.median := Age]
+dat[, age.mean := Age]
 
 # Set NA values in these columns to respective statistic
-train[is.na(age.mean), age.mean := mean.ages]
-train[is.na(age.median), age.median := median.ages]
+dat[is.na(age.mean), age.mean := mean.ages]
+dat[is.na(age.median), age.median := median.ages]
 
 # Next I need to impute the one missing Fare value
-fares <- train[, Fare]
+fares <- dat[, Fare]
 mean.fares <- mean(fares, na.rm = TRUE)
 median.fares <- median(fares, na.rm = TRUE)
 
 # Make an age.mean and age.median column
-train[, fare.mean := Fare]
-train[, fare.median := Fare]
+dat[, fare.mean := Fare]
+dat[, fare.median := Fare]
 
 # Set the one NA value in these columns to their respective imputed values
-train[is.na(fare.mean), fare.mean := mean.fares]
-train[is.na(fare.median), fare.median := median.fares]
+dat[is.na(fare.mean), fare.mean := mean.fares]
+dat[is.na(fare.median), fare.median := median.fares]
 
 
 # Next I will impute the missing Cabin values, and I will say anyone who is missing
 # a Cabin didn't have a Cabin. This is a huge topic of debate in this dataset, 
 # and deeper analysis into this problem would require a much more thoughtful 
 # imputation of these values. I'm just doing this for now.
-train[which(grepl(".", Cabin)), Cabin := "Cabin"]
-train[-which(grepl(".", Cabin)), Cabin := "No Cabin"]
+dat[which(grepl(".", Cabin)), Cabin := "Cabin"]
+dat[-which(grepl(".", Cabin)), Cabin := "No Cabin"]
 
+dat[, Cabin := factor(Cabin)]
 
-#################### Clean the test dataset ####################################
+#################### Clean the sub dataset ####################################
 
 # I want to change the Survived, and Pclass variables to factor now
 
-test[, Pclass := factor(Pclass)]
-test[, Sex := factor(Sex)]
+sub[, Pclass := factor(Pclass)]
+sub[, Sex := factor(Sex)]
 
 # The Embarked variable can be dropped, as I found it unimportant in the EDA.R
 # script
-test[, Embarked := NULL]
+sub[, Embarked := NULL]
 
 # Now I will impute the missing Age values and create two new columns for imputations. 
 # One will be a mean imputation and the other a median. 
-ages <- test[, Age]
+ages <- sub[, Age]
 mean.ages <- mean(ages, na.rm = TRUE)
 median.ages <- median(ages, na.rm = TRUE)
 
 
 # Make age.median and age.mean column
-test[, age.median := Age]
-test[, age.mean := Age]
+sub[, age.median := Age]
+sub[, age.mean := Age]
 
 # Set NA values in these columns to respective statistic
-test[is.na(age.mean), age.mean := mean.ages]
-test[is.na(age.median), age.median := median.ages]
+sub[is.na(age.mean), age.mean := mean.ages]
+sub[is.na(age.median), age.median := median.ages]
 
 # Next I need to impute the one missing Fare value
-fares <- test[, Fare]
+fares <- sub[, Fare]
 mean.fares <- mean(fares, na.rm = TRUE)
 median.fares <- median(fares, na.rm = TRUE)
 
 # Make an age.mean and age.median column
-test[, fare.mean := Fare]
-test[, fare.median := Fare]
+sub[, fare.mean := Fare]
+sub[, fare.median := Fare]
 
 # Set the one NA value in these columns to their respective imputed values
-test[is.na(fare.mean), fare.mean := mean.fares]
-test[is.na(fare.median), fare.median := median.fares]
+sub[is.na(fare.mean), fare.mean := mean.fares]
+sub[is.na(fare.median), fare.median := median.fares]
 
 # Next I will impute the missing Cabin values, and I will say anyone who is missing
 # a Cabin didn't have a Cabin. This is a huge topic of debate in this dataset, 
 # and deeper analysis into this problem would require a much more thoughtful 
 # imputation of these values. I'm just doing this for now.
-test[which(grepl(".", Cabin)), Cabin := "Cabin"]
-test[-which(grepl(".", Cabin)), Cabin := "No Cabin"]
+sub[which(grepl(".", Cabin)), Cabin := "Cabin"]
+sub[-which(grepl(".", Cabin)), Cabin := "No Cabin"]
+
+sub[, Cabin := factor(Cabin)]
+
 
 
 # Let's create two new columns that will use the turn the 
 # Parch variable into a binary variable
-train[, Parch_bin := ifelse(as.numeric(Parch) > 0, 1, 0)]
-test[, Parch_bin := ifelse(as.numeric(Parch) > 0, 1, 0)]
+dat[, Parch_bin := ifelse(as.numeric(Parch) > 0, 1, 0)]
+sub[, Parch_bin := ifelse(as.numeric(Parch) > 0, 1, 0)]
 
 
 # Let's create a new predictor which makes the Pclass predictor binary based on 
 # being in Pclass 1 or not
-train[, Pclass_bin := ifelse(as.numeric(Pclass == 1), 1, 0)]
-test[, Pclass_bin := ifelse(as.numeric(Pclass == 1), 1, 0)]
+dat[, Pclass_bin := ifelse(as.numeric(Pclass == 1), 1, 0)]
+sub[, Pclass_bin := ifelse(as.numeric(Pclass == 1), 1, 0)]
